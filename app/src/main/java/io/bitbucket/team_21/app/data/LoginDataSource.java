35c68a308398cf94b03aed747afa08cbbeb00c17
Joe Dai <dm.joedai@gmail.com>
2020-03-04 19:55:12 -0800

    Add dummy login/password into app.data.LoginDataSource

--- app/src/main/java/io/bitbucket/team_21/app/data/LoginDataSource.java
+++ app/src/main/java/io/bitbucket/team_21/app/data/LoginDataSource.java
@@ -4,20 +4,26 @@ import io.bitbucket.team_21.app.data.model.LoggedInUser;
 
 import java.io.IOException;
 
+import javax.security.auth.login.LoginException;
+
 /**
  * Class that handles authentication w/ login credentials and retrieves user information.
  */
 public class LoginDataSource {
 
     public Result<LoggedInUser> login(String username, String password) {
-
         try {
-            // TODO: handle loggedInUser authentication
-            LoggedInUser fakeUser =
-                    new LoggedInUser(
-                            java.util.UUID.randomUUID().toString(),
-                            "Jane Doe");
-            return new Result.Success<>(fakeUser);
+            // TODO: handle actual loggedInUser authentication
+            if (username.equals("user@example.com") && password.equals("password")) {
+                LoggedInUser fakeUser =
+                        new LoggedInUser(
+                                java.util.UUID.randomUUID().toString(),
+                                "Example User");
+                return new Result.Success<>(fakeUser);
+            }
+            else {
+                return new Result.Error(new LoginException("Bad username/password"));
+            }
         } catch (Exception e) {
             return new Result.Error(new IOException("Error logging in", e));
         }
