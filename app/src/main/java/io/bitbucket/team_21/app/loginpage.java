58df813fb30361b2125d54e07c907dd4ce3c040a
Trueon <tdsafine@gmail.com>
2020-03-11 18:38:19 -0700

    changed some stuff to pull from Rex.

--- app/src/main/java/io/bitbucket/team_21/app/loginpage.java
+++ app/src/main/java/io/bitbucket/team_21/app/loginpage.java
@@ -12,21 +12,4 @@ import android.view.View;
 
 public class loginpage extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_loginpage);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
-
 }
