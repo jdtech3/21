b4e8310767b5a3617920b7fe1578b678f73a56ca
Trueon <tdsafine@gmail.com>
2020-03-31 13:43:37 -0700

    pre-pull commit

--- app/src/main/java/io/bitbucket/team_21/app/statuspage.java
+++ app/src/main/java/io/bitbucket/team_21/app/statuspage.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
@@ -12,14 +13,9 @@ import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
 
 import java.lang.reflect.Array;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
-public class statuspage extends AppCompatActivity {
-
-    BarChart barChart = (BarChart) findViewById(R.id.chart1);
-
-    Integer[] dataObjects;
-
-    ArrayList<BarEntry> entries = new ArrayList<>();
+public class statuspage extends AppCompatActivity{
 
 }
