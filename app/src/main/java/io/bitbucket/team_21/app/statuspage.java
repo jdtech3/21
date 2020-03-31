06ea04b4427aab71520cf7016b4c54ad764526e2
Trueon <tdsafine@gmail.com>
2020-03-31 13:03:39 -0700

    did a few changes

---
+++ app/src/main/java/io/bitbucket/team_21/app/statuspage.java
@@ -0,0 +1,15 @@
+package io.bitbucket.team_21.app;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
+
+import java.util.ArrayList;
+
+
