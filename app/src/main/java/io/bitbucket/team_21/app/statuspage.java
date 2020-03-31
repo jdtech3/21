873ea79d0c1881d2284fe99aab6bb14981f71933
Trueon <tdsafine@gmail.com>
2020-03-31 13:04:38 -0700

    Did some changes. Attempted to add a chart — failed.

--- app/src/main/java/io/bitbucket/team_21/app/statuspage.java
+++ app/src/main/java/io/bitbucket/team_21/app/statuspage.java
@@ -8,8 +8,18 @@ import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
+public class statuspage extends AppCompatActivity {
 
+    BarChart barChart = (BarChart) findViewById(R.id.chart1);
+
+    Integer[] dataObjects;
+
+    ArrayList<BarEntry> entries = new ArrayList<>();
+
+}
