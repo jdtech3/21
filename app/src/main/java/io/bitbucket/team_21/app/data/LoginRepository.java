17dedd2b7c3602897f74995624c487b1403f38ba
Joe Dai <dm.joedai@gmail.com>
2020-04-01 20:30:08 -0700

    Oops fixed

--- app/src/main/java/io/bitbucket/team_21/app/data/LoginRepository.java
+++ app/src/main/java/io/bitbucket/team_21/app/data/LoginRepository.java
@@ -4,7 +4,7 @@ import io.bitbucket.team_21.app.data.model.LoggedInUser;
 
 /**
  * Class that requests authentication and user information from the remote data source and
- * maintains an in-memory cache of login status and user credentials information.
+ * maintains an in-memory cache of login status_icon and user credentials information.
  */
 public class LoginRepository {
 
