dc4a6070f6e6484cde99c1b560a7645f1ac5f363
Joe Dai <dm.joedai@gmail.com>
2020-04-08 20:14:58 -0700

    Force the SystemUiVisibility flags to be immersive/fullscreen mode, mwahahaha (only on homepage rn tho)

--- app/src/main/java/io/bitbucket/team_21/app/homepage.java
+++ app/src/main/java/io/bitbucket/team_21/app/homepage.java
@@ -25,6 +25,26 @@ public class homepage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // Get decorView so we can make changes to it
+        final View decorView = getWindow().getDecorView();
+
+        // Override the SystemUiVisibilityChange event listener
+        decorView.setOnSystemUiVisibilityChangeListener(
+            new View.OnSystemUiVisibilityChangeListener() {
+                @Override
+                public void onSystemUiVisibilityChange(int visibility) {
+                    // Force the flags to be immersive mode every time, mwahahaha
+                    int uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                                  | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                                  | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                                  | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                                  | View.SYSTEM_UI_FLAG_FULLSCREEN
+                                  | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+
+                    decorView.setSystemUiVisibility(uiOptions);
+                }
+            });
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
 
@@ -39,5 +59,21 @@ public class homepage extends AppCompatActivity {
         PieChartData pieChartData = new PieChartData(pieData);
         pieChartData.setHasCenterCircle(true).setCenterText1("Total Assignments").setCenterText1FontSize(20).setCenterText1Color(Color.parseColor("#000000"));
         pieChartView.setPieChartData(pieChartData);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Force the flags to be immersive mode, mwahahaha
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                      | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                      | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                      | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                      | View.SYSTEM_UI_FLAG_FULLSCREEN
+                      | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+
+        decorView.setSystemUiVisibility(uiOptions);
     }
 }
