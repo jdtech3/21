ef99ae55f5115aab021a71aa64820c258effebc9
Trueon <tdsafine@gmail.com>
2020-05-01 22:00:55 -0700

    Merge

---
+++ app/src/main/java/io/bitbucket/team_21/app/settingspage.java
@@ -0,0 +1,15 @@
+package io.bitbucket.team_21.app;
+
+public class settingspage {
+    public int hour;
+
+    public settingspage(int hour){
+        this.hour = hour;
+    }
+    public int getHour(){
+        return hour;
+    }
+    public void setHour(){
+        this.hour = hour;
+    }
+}
